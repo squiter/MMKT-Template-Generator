ecc12eac0e7df89db7f4d185ba4ddfb1d083e75c
Brunno dos Santos <squiter85@gmail.com>
2012-04-04 14:56:40 -0300

    Best way to verify is  is setting

--- includes/mtg_page_add_template.php
+++ includes/mtg_page_add_template.php
@@ -29,24 +29,24 @@
 			</div>
 	<?php endif; ?>
 	
-	<?php $action = ($template) ? "&edit=$template_id" : "";?>
+	<?php $action = (isset($template)) ? "&edit=$template_id" : "";?>
 	
 	<form action="<?php echo get_admin_url() . "admin.php?page=" . MTG_ADD_TEMPLATE_PAGE . $action?>" method="post" accept-charset="utf-8">
 		<table class="form-table">
 			<tr valign="top">
 				<th><label for="edition_number"><?php _e("Edition Number", MTG_TEXTDOMAIN);?></label></th>
 				<td>
-					<input type="text" name="edition_number" onkeyup="javascript:mask(this,numberMask);" maxlength="5" id="edition_number" value="<?php echo ($template) ? $template->edition_number : (isset($_POST["edition_number"]) ? $_POST["edition_number"] : ""); ?>" />
+					<input type="text" name="edition_number" onkeyup="javascript:mask(this,numberMask);" maxlength="5" id="edition_number" value="<?php echo (isset($template)) ? $template->edition_number : (isset($_POST["edition_number"]) ? $_POST["edition_number"] : ""); ?>" />
 				</td>
 			</tr>
 			<tr>
 				<th><label for="edition_date"><?php _e("Edition date", MTG_TEXTDOMAIN);?></label></th>
 				<td>
-					<input type="text" name="edition_date"  id="edition_date" onkeydown="javascript:mask(this,dateMask);" maxlength="10" value="<?php echo ($template) ? date("d/m/Y", strtotime($template->edition_date)) : (isset($_POST["edition_date"]) ? $_POST["edition_date"] : ""); ?>" />
+					<input type="text" name="edition_date"  id="edition_date" onkeydown="javascript:mask(this,dateMask);" maxlength="10" value="<?php echo (isset($template)) ? date("d/m/Y", strtotime($template->edition_date)) : (isset($_POST["edition_date"]) ? $_POST["edition_date"] : ""); ?>" />
 				</td>
 		</table>
-		<input type="hidden" name="created_by" value="<?php echo ($template) ? $template->create_by : $current_user->ID;?>" id="created_by" />
-		<?php if($template) : ?>
+		<input type="hidden" name="created_by" value="<?php echo (isset($template)) ? $template->create_by : $current_user->ID;?>" id="created_by" />
+		<?php if(isset($template)) : ?>
 			<input type="hidden" name="id" value="<?php echo $template->id?>" id="id" />
 		<?php endif; ?>
 		<p class="submit"><input type="submit" value="<?php _e("Save Options", MTG_TEXTDOMAIN);?>" id="submit" class="button-primary" /></p>
